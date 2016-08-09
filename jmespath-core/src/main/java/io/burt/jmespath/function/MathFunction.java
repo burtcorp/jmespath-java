368a2d5eef8d83ff9bc540bdf853a8b81c9871d1
Theo <theo@iconara.net>
2016-08-09 12:09:03 +0200

    Remove unnecessary type checking in MathFunction
    I had forgotten to remove this when I rewrote everything else.

--- jmespath-core/src/main/java/io/burt/jmespath/function/MathFunction.java
+++ jmespath-core/src/main/java/io/burt/jmespath/function/MathFunction.java
@@ -12,19 +12,9 @@ public abstract class MathFunction extends JmesPathFunction {
 
   @Override
   protected <T> T callFunction(Adapter<T> adapter, List<ExpressionOrValue<T>> arguments) {
-    ExpressionOrValue<T> argument = arguments.get(0);
-    if (argument.isExpression()) {
-      throw new ArgumentTypeException(name(), "number", "expression");
-    } else {
-      T value = argument.value();
-      JmesPathType type = adapter.typeOf(value);
-      if (type == JmesPathType.NUMBER) {
-        double n = adapter.toNumber(value).doubleValue();
-        return adapter.createNumber(performMathOperation(n));
-      } else {
-        throw new ArgumentTypeException(name(), "number", type.toString());
-      }
-    }
+    T value = arguments.get(0).value();
+    double n = adapter.toNumber(value).doubleValue();
+    return adapter.createNumber(performMathOperation(n));
   }
 
   protected abstract double performMathOperation(double n);
