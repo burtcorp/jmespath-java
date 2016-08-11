2d153f520d9e0e5b73f69a32783ecf152a5a9e19
Theo <theo@iconara.net>
2016-08-11 18:29:07 +0200

    Add API docs to FunctionArgument

--- jmespath-core/src/main/java/io/burt/jmespath/function/FunctionArgument.java
+++ jmespath-core/src/main/java/io/burt/jmespath/function/FunctionArgument.java
@@ -2,6 +2,12 @@ package io.burt.jmespath.function;
 
 import io.burt.jmespath.Expression;
 
+/**
+ * In JMESPath most functions take regular values as arguments, but some are
+ * higher order functions and take expressions. This class exists so that
+ * argument lists can have a single type, while still contain a mix of values
+ * and expressions.
+ */
 public abstract class FunctionArgument<T> {
   private static class V<U> extends FunctionArgument<U> {
     private final U value;
@@ -27,21 +33,37 @@ public abstract class FunctionArgument<T> {
     public boolean isExpression() { return true; }
   }
 
+  /**
+   * Creates a new function argument that contains a value.
+   */
   public static <U> FunctionArgument<U> of(U value) {
     return new V<U>(value);
   }
 
+  /**
+   * Creates a new function argument that contains an expression.
+   */
   public static <U> FunctionArgument<U> of(Expression<U> expression) {
     return new E<U>(expression);
   }
 
   private FunctionArgument() { }
 
+  /**
+   * Returns the expression contained in this argument, or null if this argument
+   * is not an expression argument.
+   */
   public Expression<T> expression() { return null; };
 
+  /**
+   * Returns the value contained in this argument, or null if this argument is
+   * not a value argument.
+   */
   public T value() { return null; }
 
+  /** Returns true when this argument contains an expression */
   public boolean isExpression() { return false; }
 
+  /** Returns true when this argument contains a value */
   public boolean isValue() { return false; }
 }
