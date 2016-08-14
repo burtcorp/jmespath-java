f96412d93f4d35cfadc18ae3bb8533d7ab38a438
Gustav Munkby <grddev@gmail.com>
2016-08-14 15:31:50 +0200

    Fix SliceNode rewrite
    I was sure the tests were passing, so I'm not really sure what happened.
    
    It turns out (unsurprisingly) that also the begin range needs to be adjusted for backwards traversal (to avoid `IndexOutOfBoundsException`).
    
    Also, the iteration logic required the `begin + N * step == end`, which is obviously not always true, and would omit the last element if this wasn't the case. While the end could certainly be adjusted to make everything fit, I thought it was nicer to use the number of items already computed for the ArrayList allocation. When making that change, I quickly realized that this computation is also wrong when step-sizes do not align, so rounding was added to ensure the final step is included.

--- jmespath-core/src/main/java/io/burt/jmespath/node/SliceNode.java
+++ jmespath-core/src/main/java/io/burt/jmespath/node/SliceNode.java
@@ -10,10 +10,12 @@ public class SliceNode<T> extends Node<T> {
   private final int stop;
   private final int step;
   private final int limit;
+  private final int rounding;
 
   public SliceNode(Adapter<T> runtime, Integer start, Integer stop, Integer step, Node<T> source) {
     super(runtime, source);
     this.step = (step == null) ? 1 : step;
+    this.rounding = (this.step < 0) ? (this.step + 1) : (this.step - 1);
     this.limit = (this.step < 0) ? -1 : 0;
     this.start = (start == null) ? this.limit : start;
     this.stop = (stop == null) ? ((this.step < 0) ? Integer.MIN_VALUE : Integer.MAX_VALUE) : stop;
@@ -22,11 +24,12 @@ public class SliceNode<T> extends Node<T> {
   @Override
   public T searchOne(T projectionElement) {
     List<T> elements = runtime.toList(projectionElement);
-    int begin = (start < 0) ? Math.max(elements.size() + start, 0) : Math.min(start, elements.size());
+    int begin = (start < 0) ? Math.max(elements.size() + start, 0) : Math.min(start, elements.size() + limit);
     int end = (stop < 0) ? Math.max(elements.size() + stop, limit) : Math.min(stop, elements.size());
-    List<T> output = new ArrayList<>(Math.max(0, (end - begin) / step));
-    for (int i = begin; i != end; i += step) {
-      output.add(elements.get(i));
+    int steps = Math.max(0, (end - begin + rounding) / step);
+    List<T> output = new ArrayList<>(steps);
+    for (int i = 0, offset = begin; i < steps; i++, offset += step) {
+      output.add(elements.get(offset));
     }
     return runtime.createArray(output);
   }
