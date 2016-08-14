d27ca62d291dca9b28e0f5628815cd0f5709707f
Gustav Munkby <grddev@gmail.com>
2016-08-14 13:26:40 +0200

    Reimplement SliceNode
    This changes the core of the SliceNode logic to perform one simple loop, where elements are successively added to an ArrayList. The number of elements is precomputed as to avoid intermediate allocations of the array list.
    
    The previous logic was rewritten using ?: expressions in order to highlight the similarities (and differences) between start and stop.
    If we ignore the default values for start and end for a moment, it turns out that the only difference between forward and backward iteration is that the lower limit for then end for backward iteration is -1 and not
    zero.
    
    In order to initialize the default values, forward iteration is simple, as we can just use 0 and `Integer.MAX_VALUE` as the defaults. For backward iteration, we can use -1 as the start, as that will always be translated to `elements.size() - 1`, but the stop is a bit tricker as we would need a value N such that `elements.size() + N` is always negative. Thanks to two-complement representation of integers `Integer.MIN_VALUE` fits the bill, as it happens to be more negative than `Integer.MAX_VALUE` is positive, and we cannot have a `List<T>` that is longer than `Integer.MAX_VALUE`.
    
    I decided to inline handling of default values in the constructor, which  I guess technically changes equals so that `SliceNode(runtime, 0, 10, 1, source)` is equal to `SliceNode(runtime, null, 10, 1, source)`, but they will behave in the same way, so I guess it doesn't really matter.
    
    It also affects the toString method, as it will not print the computed default values instead of `null` for those cases. I guess both fields could be retained if this identity is important.
    
    I also removed the protected accessor methods, as they only seemed to be used inside the class, and there it seems to make more sense to use the fields directly.

--- jmespath-core/src/main/java/io/burt/jmespath/node/SliceNode.java
+++ jmespath-core/src/main/java/io/burt/jmespath/node/SliceNode.java
@@ -1,67 +1,34 @@
 package io.burt.jmespath.node;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.LinkedList;
-import java.util.Collections;
 
 import io.burt.jmespath.Adapter;
 
 public class SliceNode<T> extends Node<T> {
-  private final Integer start;
-  private final Integer stop;
-  private final Integer step;
+  private final int start;
+  private final int stop;
+  private final int step;
+  private final int limit;
 
   public SliceNode(Adapter<T> runtime, Integer start, Integer stop, Integer step, Node<T> source) {
     super(runtime, source);
-    this.start = start;
-    this.stop = stop;
-    this.step = step;
+    this.step = (step == null) ? 1 : step;
+    this.limit = (this.step < 0) ? -1 : 0;
+    this.start = (start == null) ? this.limit : start;
+    this.stop = (stop == null) ? ((this.step < 0) ? Integer.MIN_VALUE : Integer.MAX_VALUE) : stop;
   }
 
   @Override
   public T searchOne(T projectionElement) {
     List<T> elements = runtime.toList(projectionElement);
-    List<T> output = new LinkedList<>();
-    int i;
-    int n;
-    if (start == null) {
-      i = step > 0 ? 0 : elements.size();
-    } else if (start < 0) {
-      i = elements.size() + start;
-    } else {
-      i = start;
-    }
-    if (stop == null) {
-      n = step > 0 ? elements.size() : -1;
-    } else if (stop < 0) {
-      n = elements.size() + stop;
-    } else {
-      n = Math.min(elements.size(), stop);
-    }
-    if (step > 0) {
-      for ( ; i < n; i += step) {
-        output.add(elements.get(i));
-      }
-    } else {
-      i = Math.min(i, elements.size() - 1);
-      n = Math.max(n, -1);
-      for ( ; i > n; i += step) {
-        output.add(elements.get(i));
-      }
+    int begin = (start < 0) ? Math.max(elements.size() + start, 0) : Math.min(start, elements.size());
+    int end = (stop < 0) ? Math.max(elements.size() + stop, limit) : Math.min(stop, elements.size());
+    List<T> output = new ArrayList<>(Math.max(0, (end - begin) / step));
+    for (int i = begin; i != end; i += step) {
+      output.add(elements.get(i));
     }
     return runtime.createArray(output);
-  }
-
-  protected Integer start() {
-    return start;
-  }
-
-  protected Integer stop() {
-    return stop;
-  }
-
-  protected Integer step() {
-    return step;
   }
 
   @Override
@@ -72,7 +39,7 @@ public class SliceNode<T> extends Node<T> {
   @Override
   protected boolean internalEquals(Object o) {
     SliceNode other = (SliceNode) o;
-    return start() == other.start() && stop() == other.stop() && step() == other.step();
+    return start == other.start && stop == other.stop && step == other.step;
   }
 
   @Override
