bf60792669752f4ffa8d60ee96315b66c96c0f16
Theo <theo@iconara.net>
2016-08-25 15:28:56 +0200

    Change JsonLiteralNode to parse its argument
    Mostly to make it work like StringNode and PropertyNode.

--- jmespath-core/src/main/java/io/burt/jmespath/node/JsonLiteralNode.java
+++ jmespath-core/src/main/java/io/burt/jmespath/node/JsonLiteralNode.java
@@ -3,35 +3,33 @@ package io.burt.jmespath.node;
 import io.burt.jmespath.Adapter;
 
 public class JsonLiteralNode<T> extends Node<T> {
-  private final String raw;
-  private final T tree;
+  private final String rawJson;
+  private final T json;
 
-  public JsonLiteralNode(Adapter<T> runtime, String raw, T tree) {
+  public JsonLiteralNode(Adapter<T> runtime, String rawJson) {
     super(runtime);
-    this.raw = raw;
-    this.tree = tree;
+    this.rawJson = rawJson;
+    this.json = runtime.parseString(rawJson);
   }
 
   @Override
   public T search(T input) {
-    return tree;
+    return json;
   }
 
   @Override
   protected String internalToString() {
-    return String.format("%s", raw);
+    return String.format("%s", rawJson);
   }
 
   @Override
   protected boolean internalEquals(Object o) {
     JsonLiteralNode<?> other = (JsonLiteralNode<?>) o;
-    return tree.equals(other.tree);
+    return json.equals(other.json);
   }
 
   @Override
   protected int internalHashCode() {
-    int h = 1;
-    h = h * 31 + tree.hashCode();
-    return h;
+    return json.hashCode();
   }
 }
