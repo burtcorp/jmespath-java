af0ed31b7106493292a9f83553ae9bc79b6c8a21
Győző Papp <gyozo.papp@meltwater.com>
2018-07-27 01:03:25 +0200

    normalize_space() expects string argument only

--- jmespath-core/src/main/java/io/burt/jmespath/function/NormalizeSpaceFunction.java
+++ jmespath-core/src/main/java/io/burt/jmespath/function/NormalizeSpaceFunction.java
@@ -3,10 +3,16 @@ package io.burt.jmespath.function;
 import java.util.List;
 
 import io.burt.jmespath.Adapter;
+import io.burt.jmespath.JmesPathType;
 
 public class NormalizeSpaceFunction extends BaseFunction {
+  /**
+   * The zero-argument form of this function is not supported
+   * since it is just a shorthand of using the current context
+   * ('.' in XPath and '@' in JmesPath)
+   */
   public NormalizeSpaceFunction() {
-    super(ArgumentConstraints.anyValue());
+    super(ArgumentConstraints.typeOf(JmesPathType.STRING));
   }
 
   @Override
